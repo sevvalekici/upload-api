358653a3bfb585cb74b9f4b5204535728dea7d1b
Başak Şevval Ekici <sevval.ekici@gmail.com>
2021-09-14 15:52:41 +0300

    index change location

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const config = require('./config')
+const userRouter = require('./src/routers/user')
+const mediaRouter = require('./src/routers/media')
+const handlerRouter = require('./src/routers/handler')
+
+app.use(express.json())
+// to support urlencoded data
+app.use(express.urlencoded({extended: true}))
+app.use(userRouter)
+app.use(mediaRouter)
+app.use(handlerRouter)
+
+const port = process.env.PORT || 3000
+const hostname = process.env.HOST || '127.0.0.1'
+app.listen(port, hostname, () => {
+    console.log(`listening on port ${
+        config.hosting.port
+    }`)
+})
